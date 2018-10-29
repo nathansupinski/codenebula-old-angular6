224ece301a960a15b34fc5d733ba30ef69784469
Nathan Supinski <nathan@supinski.net>
2018-10-28 23:17:56 -0600

    user auth skeleton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'codenebula';
-  //test change to trigger test run
+  // test change to trigger test run
 }
---
+++ src/app/login.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-component',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  title = 'codenebula';
+  // test change to trigger test run
+}

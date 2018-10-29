bdf78e6b765516ac300dee18cdc627cea31d6026
Nathan Supinski <nathan@supinski.net>
2018-10-28 23:32:12 -0600

    require registration code to register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -24,6 +24,7 @@ export class RegisterComponent implements OnInit {
             username: ['', Validators.required],
             password: ['', [Validators.required, Validators.minLength(6)]],
             email: ['', Validators.required],
+            regCode: ['', Validators.required]
         });
     }
 

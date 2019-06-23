c8bd989729a8e82da21ca214f6cb02b427b008ff
Nathan Supinski <nathan@supinski.net>
2019-06-23 00:45:38 -0600

    build pipeline - fix builds

--- src/app/homeCarousel/homeCarousel.component.ts
+++
@@ -1,6 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({selector: 'app-home-carousel-basic', templateUrl: './homeCarousel.component.html'})
-export class AppHomeCarouselBasic {
-  images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
-}
